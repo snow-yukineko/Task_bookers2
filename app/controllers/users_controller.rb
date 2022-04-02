43eab25e9fa4aaa880c8869d1f7297a8e6f63d08
snow-yukineko <mutsumi141208@gmail.com>
2022-04-02 13:55:09 +0000

    [Add] bookers2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,23 @@
 class UsersController < ApplicationController
   def show
+    @user = User.find(params[:id])
+    @books = @user.books
+    @book = Book.new
+  end
+  
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to books_path
   end
 
   def edit
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_imafe)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/show'
-  get 'users/edit'
-  get 'books/new'
-  get 'books/index'
-  get 'books/show'
   devise_for :users
   root to: 'homes#top'
+  resources :books, only: [:new, :index, :show]
+  resources :users, only: [:show, :edit]
   get 'homes/about'
+  post 'books' => 'books#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
